da58471eac4ee7066f327ddac656d3b22f535443
Flix <flixgit@outlook.com>
2025-05-19 09:21:24 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'vplaza.mollypornstar.com/bare/',
+    bare:'vplaza.org/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
